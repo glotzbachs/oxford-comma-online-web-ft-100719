87405372005bce330f4dd6b18653aa74fdc82d65
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-08 19:17:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,5 @@ def oxford_comma(array)
  last=array[-1]
  less=array.pop
  start=less.join(", ")
+ final=start+", and "+last
 end
-
-
- array.join(", ")
-  number=array.length
-  array[number-2]="and"
-  array
